042206da40e58d54483ad3ff7f51d023746da8ad
KoharuYuzuki <79073521+KoharuYuzuki@users.noreply.github.com>
2022-09-02 16:30:58 +0900

    例を追加

---
+++ example.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const openJTalk = require('./js/open_jtalk_modified.js');
+
+openJTalk.run({
+  text:  'こんにちは',
+  dict:  'path/to/dict_dir',
+  voice: 'path/to/htsvoice_file',
+  speed: 1.0,
+  rate:  48000,
+  type:  'token', // token or wav
+  tmp:   'path/to/tmp_dir'
+}, (result) => {
+  console.log(result);
+});
